29a5dcef14ce3337493698ce385bdd18c77b3881
Elliott Magnuson <ecmagnuson@wisc.edu>
2023-01-29 21:35:55 -0600

    Create save image method

--- src/main/java/mosaic/Main.java
+++ src/main/java/mosaic/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 import javax.imageio.ImageIO;
 
 public class Main {
-	
+
 	// return a List of all Block objects in /blocks dir
 	public static List<Block> getBlocks() throws IOException {
 		var blockFile = new File(System.getProperty("user.dir") + "/blocks/");
@@ -28,7 +28,6 @@ public class Main {
 	public static List<UserImage> getImages() throws IOException {
 		var imageFile = new File(System.getProperty("user.dir") + "/inputimages/");
 		var images = new ArrayList<UserImage>();
-		// fileName.substring(fileName.lastIndexOf('.'))
 		for (File f : imageFile.listFiles()) {
 			if (f.getName().equals(".gitignore")) {
 				continue;
@@ -51,14 +50,11 @@ public class Main {
 		return (int) Math.sqrt(Math.pow((r1 - r2), 2) + Math.pow((g1 - g2), 2) + Math.pow((b1 - b2), 2));
 	}
 
-	// match the Color of a pixel to it's closest Block.avgRGBColor
-	// return the Color of that Block
-	
-	//return the Color of the Block that has the closest RGB value to a Color pixel
+	// return the Color of the Block that has the closest RGB value to a Color pixel
 	public static Color matchPixelToBlock(Color pixel, List<Block> blocks) {
 		var match = new Color(0, 0, 0, 255);
 		int minDist = calculateDiff2Colors(pixel, match);
-		//for each block see how close it is to this color, return that color
+		// for each block see how close it is to this color, return that color
 		for (var block : blocks) {
 			int dist = calculateDiff2Colors(pixel, block.getAvgRGBColors());
 			if (dist < minDist) {
@@ -80,6 +76,13 @@ public class Main {
 		return image;
 	}
 
+	// save the newly created mosaic to the /output directory
+	public static void saveMinecraftImage(BufferedImage minecraftMosaic, String name) throws IOException {
+		Files.createDirectories(Paths.get(System.getProperty("user.dir") + "/output"));
+		var output = new File(System.getProperty("user.dir") + "/output/" + "Minecraft" + name);
+		ImageIO.write(minecraftMosaic, "png", output);
+	}
+
 	public static void transform(List<UserImage> userImages, List<Block> blocks) throws IOException {
 		for (var ui : userImages) {
 			BufferedImage minecraftMosaic = createCanvas(16 * ui.image().getWidth(), 16 * ui.image().getHeight(),
@@ -88,8 +91,7 @@ public class Main {
 				for (int y = 0; y < ui.image().getHeight(); y++) {
 					int pixel = ui.image().getRGB(x, y);
 					var pixelColor = new Color(pixel, true);
-					//Find the color that matches a Block value that is closest to the image Pixel
-					//
+					// Find the color that matches a Block value that is closest to the image Pixel
 					Color match = matchPixelToBlock(pixelColor, blocks);
 					var blockImage = Block.color2image.get(match);
 					if (blockImage == null) {
@@ -102,18 +104,16 @@ public class Main {
 					blockToPaste.drawImage(blockImage, null, x * 16, y * 16);
 				}
 			}
-			Files.createDirectories(Paths.get(System.getProperty("user.dir") + "/output"));
-			var output = new File(System.getProperty("user.dir") + "/output/" + "Minecraft" + ui.name());
-			ImageIO.write(minecraftMosaic, "png", output);
+			saveMinecraftImage(minecraftMosaic, ui.name());
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
-		List<Block> blocks = getBlocks();
 		List<UserImage> images = getImages();
+		List<Block> blocks = getBlocks();
 		transform(images, blocks);
-		
+
 		System.out.println("Done :)");
-		System.out.println(String.format("all images (%d) are in /output", images.size()));
+		System.out.printf("all images (%d) are in /output", images.size());
 	}
 }
